606e76531194f7e62bc97cd72fb6cbc10122d6bf
Andrei Lesnitsky <andrei.lesnitsky@gmail.com>
2021-03-04 13:35:26 +0300

    add headers

--- functions/version.ts
+++ functions/version.ts
@@ -1,8 +1,15 @@
 import { APIGatewayProxyHandler } from "aws-lambda";
+import { ContentType, cors } from "../util/headers";
 
 export const version: APIGatewayProxyHandler = async (e, _) => {
   return {
+    headers: {
+      ...cors(e.headers["origin"]!),
+      ...ContentType.JSON,
+    },
     statusCode: 200,
-    body: JSON.stringify({ version: require("../package.json").version }),
+    body: JSON.stringify({
+      version: require("../package.json").version,
+    }),
   };
 };
