16c92b8a405a6e467ad927d7e6db98e6eb414744
Andrei Lesnitsky <andrei.lesnitsky@gmail.com>
2021-03-25 15:52:01 +0300

    add node externals

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,10 @@
 const path = require("path");
+const nodeExternals = require("webpack-node-externals");
 
 module.exports = {
   entry: "./handler.ts",
   target: "node",
+  externals: [nodeExternals()],
   mode: process.env.NODE_ENV,
   module: {
     rules: [
